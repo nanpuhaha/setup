93ddb68f6b8ce38648d5f736eb3136afa3da5104
Seo, Jang-won <nanpuhaha@users.noreply.github.com>
2023-10-26 16:17:22 +0900

    Create winget.ps1

---
+++ Windows/winget.ps1
@@ -0,0 +1,23 @@
+# 패키지 검색하기
+# winget search
+
+
+# 설치된 패키지 목록 보기
+# winget list 
+winget install GitHub.GitHubDesktop
+winget install SublimeHQ.SublimeText.4
+
+
+winget install NGWIN.PicPick  # PicPick은 버전이 좀 느림
+winget install Notion.Notion
+winget install Bandisoft.Bandizip
+winget install 9PH84MZPJ931 # Send Anywhere - Make file transfers faster
+winget install voidtools.Everything
+winget install Apple.iTunes
+winget install Automattic.Simplenote
+winget install WinMerge.WinMerge
+winget install Microsoft.VisualStudioCode
+winget install Microsoft.VisualStudioCode.Insiders
+winget install 
+winget install 
+winget install 
