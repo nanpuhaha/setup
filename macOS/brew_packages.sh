b84eebbf0fbfda6ba251b932ac69efc0537ce037
Seo, Jang-won <nanpuhaha@users.noreply.github.com>
2023-12-12 11:01:54 +0900

    Update brew_packages.sh

--- macOS/brew_packages.sh
+++ macOS/brew_packages.sh
@@ -83,3 +83,93 @@ brew install --cask kicad
 # https://github.com/serhii-londar/open-source-mac-os-apps
 # https://github.com/mathiasbynens/dotfiles
 
+
+
+
+
+brew install neovim  # Text Editor
+brew install git  # Version Control
+brew install tmux  # Terminal Multiplexer
+brew install htop  # Process Manager
+brew install python3  # Scripting Language
+
+brew install trash # Trash helper
+brew install tree # Recursive directory lister
+brew install rename # Batch rename tool
+brew install fzf # Fuzzy finder
+brew install jq # JSON processor
+brew install rlwrap  # Readline wrapper
+
+
+
+
+
+brew install openjdk (Java)
+
+brew install node (Node.js)
+brew install yarn
+brew install coreutils # GNU File, Shell, and Text utilities
+brew install pkg-config # Manage compile and link flags for libraries
+brew install automake #  Tool for generating GNU Standards-compliant Makefiles
+
+
+libtool: Generic library support script
+cmake: Cross-platform make
+readline: Library for command-line editing
+libyaml: YAML Parser
+autoconf: Automatic configure script builder
+redis: Persistent key-value database, with built-in net interface
+nmap: Port scanning utility for large networks
+pyenv: Python version management
+ansible: Automate deployment, configuration, and upgrading
+sqlite: Command-line interface for SQLite
+watch: Executes a program periodically, showing output fullscreen
+
+httpie: User-friendly cURL replacement (command-line HTTP client)
+docker-compose: Isolated development environments using Docker
+rust: Safe, concurrent, practical language
+exiftool: Perl lib for reading and writing EXIF metadata
+aria2: Download with resuming and segmented downloading
+
+
+brew install ghidra (Reverse engineering tools)
+brew install gh (GitHub CLI)
+brew install 
+brew install blackhole-2ch (Cask) (Virtual Audio Driver, useful for routing desktop audio)
+brew install ffmpeg (Audio/video converter)
+brew install blender (Cask) (3D modeling)
+brew install obs (Cask) (Broadcasting and screencasting)
+
+
+brew install prusaslicer (Cask) (3D slicer)
+brew install openscad (Cask) (Programmable CAD modeler)
+
+brew install wget
+
+brew install mas
+brew install imagemagick
+brew install zsh
+brew install speedtest-cli
+brew install cask
+
+
+postgresql (Database)
+
+qemu (Emulator)
+utm (Cask) (Virtual machines UI on top of QEMU)
+
+
+
+docker (Cask) (Docker Desktop, container builder and runner)
+kubernetes-cli (Kubernetes API client)
+k9s (Kubernetes TUI)
+helm (Kubernetes package manager)
+
+barrier (Cask) (KVM)
+discord (Cask) (Voice and Text Chat)
+slack (Cask) (Text Chat)
+zoom (Cask) (Video Conferencing)
+
+
+https://github.com/fwcd/brew-pkgs
+https://gist.github.com/pmkay/e09034971b6f23214fd9f95a3e6d1c44
